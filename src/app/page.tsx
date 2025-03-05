2404f8e96f729d7d6b379fcb46a9d80d6253edce
dsquare0601 <darjidhaval19@gmail.com>
2025-03-05 18:32:58 +0530

    Basic navigation set-up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import Navigation from "./navigation";
 
 export default function Home() {
   return (
     <div className="px-50">
-      <main className=""></main>
+      <main className="">
+        <Navigation />
+      </main>
       <footer className=""></footer>
     </div>
   );
