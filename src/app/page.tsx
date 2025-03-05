08ffe723dabf2bb525480b1d8036c18a16d789bd
dsquare0601 <darjidhaval19@gmail.com>
2025-03-05 22:45:38 +0530

    Removed shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
 import Image from "next/image";
-import Navigation from "./navigation";
 
 export default function Home() {
   return (
     <div className="px-50">
-      <main className="">
-        <Navigation />
-      </main>
+      <main className=""></main>
       <footer className=""></footer>
     </div>
   );
