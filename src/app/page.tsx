a64248ba560a5954639b548f62b6b7d0f2c378e6
dsquare0601 <darjidhaval19@gmail.com>
2025-05-10 19:49:32 +0530

    Separate Header component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -80,6 +80,7 @@ import { SectionCarousel } from "@/components/section-carousel";
 import navbarData from "@/context/navbar.data";
 import { motion, useInView } from "framer-motion";
 import { AnimatedWords } from "@/components/animated-words";
+import Header from "@/components/Header";
 
 // Add these styles right after imports
 const textGradients = {
@@ -126,21 +127,7 @@ export default function Page() {
     <SidebarProvider>
       <AppSidebar setSection={handleSectionChange} currentSection={section} />
       <SidebarInset>
-        <header className="3xl:h-16 4xl:h-16 5xl:h-22 6xl:h-24 sticky top-0 z-50 flex h-16 shrink-0 items-center gap-2 sm:h-16 md:h-16 lg:h-12 xl:h-12 2xl:h-16">
-          <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
-            <Separator orientation="vertical" className="mr-2 h-4" />
-            <Breadcrumb>
-              <BreadcrumbList>
-                <BreadcrumbItem>
-                  <BreadcrumbPage className="3xl:text-xl 4xl:text-xl 5xl:text-xl 6xl:text-3xl text-sm sm:text-sm md:text-base lg:text-base xl:text-base 2xl:text-xl">
-                    {sectionTitle}
-                  </BreadcrumbPage>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
-          </div>
-        </header>
+        <Header sectionTitle={sectionTitle} />
 
         <SectionCarousel
           setSection={handleSectionChange}
---
+++ src/components/Header.tsx
@@ -0,0 +1,28 @@
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import { Separator } from "@/components/ui/separator";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbList,
+  BreadcrumbPage,
+} from "@/components/ui/breadcrumb";
+
+export default function Header({ sectionTitle }: { sectionTitle: string }) {
+  return (
+    <header className="3xl:h-16 4xl:h-16 5xl:h-22 6xl:h-24 sticky top-0 z-50 flex h-16 shrink-0 items-center gap-2 sm:h-16 md:h-16 lg:h-12 xl:h-12 2xl:h-16">
+      <div className="flex items-center gap-2 px-4">
+        <SidebarTrigger className="-ml-1" />
+        <Separator orientation="vertical" className="mr-2 h-4" />
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbPage className="3xl:text-xl 4xl:text-xl 5xl:text-xl 6xl:text-3xl text-sm sm:text-sm md:text-base lg:text-base xl:text-base 2xl:text-xl">
+                {sectionTitle}
+              </BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+      </div>
+    </header>
+  );
+}
